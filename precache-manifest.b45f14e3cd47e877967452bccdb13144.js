1184a65ebab243c11ef0ac70fc65b393e039962a
Riswanto <37138545+wansr87@users.noreply.github.com>
2020-04-22 20:35:05 +0700

    Updates

---
+++ precache-manifest.b45f14e3cd47e877967452bccdb13144.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93077d9718ef2b7ac72615a087f99564",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "62945791759adcf5989c",
+    "url": "/react/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "3ee3ab5b598f3ee3c5fe",
+    "url": "/react/static/js/2.8d15d1cc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react/static/js/2.8d15d1cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62945791759adcf5989c",
+    "url": "/react/static/js/main.5e2abfd8.chunk.js"
+  },
+  {
+    "revision": "28801317d80f519e4722",
+    "url": "/react/static/js/runtime-main.096ca434.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react/static/media/logo.5d5d9eef.svg"
+  }
+]);
